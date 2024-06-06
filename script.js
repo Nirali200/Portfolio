7cfcaa94b2165aa0b659439a779030ae055830b6
Nirali200 <darjinirali08@gmail.com>
2024-06-07 00:22:29 +0530

    Dynamically Quotes Fatch added

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ for(let i=0;i<lol.length;i++){
     }
 }
 
+var YOUR_API_KEY = "vA1NYmJf3Y0VztGDCoUSfoGaEECCWbo9JaCfKlzB";
+var category = 'happiness';
+
+var getQuotes = () => {
+    fetch('https://api.api-ninjas.com/v1/quotes?category=' + category,
+        { method: 'GET',
+         headers: { 'X-Api-Key': YOUR_API_KEY },
+         contentType: 'application/json'})
+    .then(Response => Response.json())
+    .then(data => addData(data));
+}
+
+const addData = (data) =>{
+   var text = data[0].quote;
+   var quote = document.querySelector('.mainHeading');
+   quote.innerHTML = "' " + text +" '";
+}
