e0a8f83d55c2d72c25fc78d0552dbaf5ac46d789
Nirali200 <darjinirali08@gmail.com>
2024-06-16 13:43:21 +0530

    structur the directories

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const pathname = window.location.pathname;
-const lol = document.querySelector('.right').childNodes[1].childNodes;
-for(let i=0;i<lol.length;i++){
-    if(i%2==1){
-        if(lol[i].childNodes[0].href.includes(pathname)){
-            lol[i].childNodes[0].classList.add('active')
-        }
-    }
-}
 
 var YOUR_API_KEY = "vA1NYmJf3Y0VztGDCoUSfoGaEECCWbo9JaCfKlzB";
 var category = 'happiness';
