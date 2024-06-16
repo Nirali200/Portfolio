cd6791f9f11ae4f628394d2c2099b9df010d2fc6
Nirali200 <darjinirali08@gmail.com>
2024-06-16 22:04:15 +0530

    Skills and Servicies added

--- script.js
+++ script.js
@@ -25,3 +25,13 @@ window.onload = ()=>{
 
     
 }
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const progressBars = document.querySelectorAll('.progress');
+
+    progressBars.forEach(bar => {
+        const percentage = bar.getAttribute('data-progress');
+        bar.style.width = percentage;
+    });
+});
